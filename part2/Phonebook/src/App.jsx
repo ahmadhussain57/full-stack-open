4acb93868ab0d30b2b2b8f0e624110e2f91c2265
Ahmad Hussain <ahmadhussain5760@gmail.com>
2026-09-25 02:51:02 +0300

    part2 section b phonebook project ex 2.6

---
+++ part2/Phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleAddName=(event)=>{
+    event.preventDefault()
+    const objectName={
+      name:newName
+    }
+    setPersons(persons.concat(objectName))
+    setNewName('')
+  }
+const handleNewNAme=(event)=>{
+ setNewName(event.target.value)
+
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handeleAddName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNewNAme}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person=> 
+          <li key={person.name}>{person.name}</li>
+        )}
+      
+      </ul>
+
+      </div>
+  )
+}
+
+export default App
