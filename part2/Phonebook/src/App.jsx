d21e950b32edf1f28de68debff28a35ec3687fa7
Ahmad Hussain <ahmadhussain5760@gmail.com>
2026-09-25 04:05:02 +0300

    part2 section b phonbook project ex 2.9

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -2,28 +2,47 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-
+  const[newNumber,setNewNumber]=useState("")
+  const [filterBy,setFilterBy]=useState('')
   const handleAddName=(event)=>{
     event.preventDefault()
-    const objectName={
-      name:newName
+    const personObject={
+      name:newName,
+      number:newNumber,
+      id:persons.length+1
     }
     const hasName=persons.some(person=>person.name===newName)
     if(!hasName){
-       setPersons(persons.concat(objectName))
+       setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
     }
     else{
       alert(`${newName} is already added to phonebook`)
     }
    
   }
+
+  const personFilter=persons.filter(person=>person.name.toLowerCase().includes(filterBy.toLowerCase()))
+console.log(personFilter)
 const handleNewNAme=(event)=>{
  setNewName(event.target.value)
 
+}
+
+const handleNewNumber=(e)=>{
+ setNewNumber(e.target.value)
+
+}
+
+const handleChangeFilterBy=(event)=>{
+  setFilterBy(event.target.value)
 }
 
   return (
@@ -35,15 +54,26 @@ const handleNewNAme=(event)=>{
           value={newName}
           onChange={handleNewNAme}
           />
+
+          Number:<input
+           type="text"
+           value={newNumber}
+          onChange={handleNewNumber}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+  
+      filter by name <input type="text" value={filterBy} onChange={handleChangeFilterBy}/>
+
+    
       <ul>
-        {persons.map(person=> 
-          <li key={person.name}>{person.name}</li>
+
+        {personFilter.map(person=> 
+          <li key={person.name}>user name: {person.name} phone number {person.number} id is{person.id} </li>
         )}
       
       </ul>
